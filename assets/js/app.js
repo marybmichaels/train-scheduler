f76458993b48b1484fb6e3bb9205f46471eb3459
MaryB <marybmichaels@outlook.com>
2019-02-02 19:52:22 -0600

    html & css

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAkLn65PNdznyYzWDGvCrRNPiLh44ygeBg",
+    authDomain: "hw7train-scheduler.firebaseapp.com",
+    databaseURL: "https://hw7train-scheduler.firebaseio.com",
+    projectId: "hw7train-scheduler",
+    storageBucket: "hw7train-scheduler.appspot.com",
+    messagingSenderId: "2758595701"
+  };
+  firebase.initializeApp(config);
+
+// initiate variables
+var name; 
+var destination; 
+var time;
+var frequency;
+
+// when "add-train" is clicked
+$()
+
+// send data newest data to firebase
+// push new data to the table
+
+
+
+})
