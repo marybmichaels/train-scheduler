b4b8099f31b69d142b4c27b1a83d0e5a61eb7013
MaryB <marybmichaels@outlook.com>
2019-02-02 23:07:03 -0600

    database = working & updating DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,18 +10,85 @@ $(document).ready(function() {
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+
+
+
 // initiate variables
-var name; 
-var destination; 
-var time;
-var frequency;
+var name = ""; 
+var destination = ""; 
+var time = "";
+var frequency = "";
 
 // when "add-train" is clicked
-$()
+$('#add-train').on('click', function(){
 
-// send data newest data to firebase
-// push new data to the table
+    // add values to variables
+    name = $('#name-input').val();
+    destination = $('#dest-input').val();
+    time = $('#time-input').val();
+    frequency = $('#freq-input').val();
 
+    console.log("name: "+ name, "dest: " + destination,"time: " + time, "freq: " + frequency);
 
+    // clear values of each input
+    $('#name-input').val("");
+    $('#dest-input').val("");
+    $('#time-input').val("");
+    $('#freq-input').val("");
+
+    // send newest data to firebase
+    database.ref().push({
+        name,
+        destination,
+        time,
+        frequency
+    });
+
+    
+})
+
+// take all existing firebase data & show it in the table
+
+// anytime something is added to database, show it in the table
+database.ref().on("value", function(snapshot) {
+    $('#show-trains').empty()
+
+    console.log(snapshot.val());
+    // Set the local variables for highBidder equal to the stored values in firebase.
+    //   console.log(child);
+    // Change the HTML to reflect the local value in firebase
+  snapshot.forEach(function(childSnapshot){
+      
+      var newName = childSnapshot.val().name;
+      var newDest = childSnapshot.val().destination;
+      var newTime = childSnapshot.val().time;
+      var newFreq = childSnapshot.val().frequency;
+      
+      console.log(newName, newDest, newTime, newFreq);
+    //   calculate next arrival
+
+    //   calculate minutes away
+      
+    //   outline layout for where data goes in table
+      var showName = $('<th scope="row">').text(newName);
+      var showDest = $('<td>').text(newDest);
+      var showTime = $('<td>').text(newTime);
+      var showFreq = $('<td>').text(newFreq);
+      var showdist = $('<td>').text("x");
+      
+      
+      // upate the DOM w/ info
+      var row = $('<tr>').append(showName, showDest, showTime, showFreq);
+      $('#show-trains').prepend(row);
+     
+        });
+    },
+    
+    // If any errors are experienced, log them to console.
+    function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+    });
 
 })
